a5f1cb932eafc2c1a9ac85fdf956cca6c5934bb3
rizal <rizal@lomotif.com>
2022-01-03 19:01:47 +0700

    feat: optimize db

--- database/database.go
+++ database/database.go
@@ -15,7 +15,11 @@ func InitDatabase(config configs.Conf) error {
 	var err error
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.MysqlUser,
 		config.MysqlPassword, config.MysqlHost, config.MysqlDBName)
-	DBConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DBConn, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
+
 	if err != nil {
 		return err
 	}
